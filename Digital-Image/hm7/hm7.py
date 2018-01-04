e5ba13f4b0dab42a54472b1ea90d0cfa0d9dcab3
M4x <aboultraman@gmail.com>
2018-01-05 06:14:21 +0800

    hm7

--- Digital-Image/hm7/hm7.py
+++ Digital-Image/hm7/hm7.py
@@ -29,54 +29,41 @@ class MorphologyDealing(object):
     def erosionImg(self, img):
         erosion = cv2.erode(img, self.kernel5, 1)
         self.show('erosion', 'original', 'erosion', erosion)
-        return cp(erosion)
         
     def dilateImg(self, img):
         diatex = cv2.dilate(img, self.kernel5, 1)
         self.show("dilate", "original", "diatex", diatex)
-        return cp(diatex)
         
     def ExopenImg(self, img):
         moropen = cv2.morphologyEx(img, cv2.MORPH_OPEN, self.kernel15)
         self.show("open op", "original", "MORPH_OPEN", moropen)
-        return cp(moropen)
         
     def ExcloseImg(self, img):
         morclose = cv2.morphologyEx(img, cv2.MORPH_CLOSE, self.kernel15)
         self.show("close op", "original", "MORPH_CLOSE", morclose) 
-        return cp(morclose)
         
     def Skeletionize(self, img):
         sk_img = sm.skeletonize(img)
         self.show("get skeletonize", "original", "skeletonize", sk_img)
-        return cp(sk_img)
         
     def Hull(self, img):
         hull_img = sm.convex_hull_image(img)
         self.show("get convex hull", "original", "convex_hull_image", hull_img)
-        return cp(hull_img)
         
     def Labeled(self, img):
         label_img = sm.label(img, neighbors=8)
         self.show("get labels", "original", "labeled_image", label_img)
-        return cp(label_img)
         
     def Thin(self, img):
         thin_img = sm.thin(img, max_iter=None)
         self.show("get thin", "original", "thin_image", thin_img)
-        return cp(thin_img)
         
     def Thick(self, img):
         w, h = img.shape
-        for i in xrange(w):
-            for j in xrange(h):
-                img[i, j] = 1 - img[i, j]
+        img = 1 - img
         thick_img = sm.thin(img, max_iter=None)
-        for i in xrange(w):
-            for j in xrange(h):
-                thick_img[i, j] = not thick_img[i, j]
+        thick_img = np.invert(thick_img)
         self.show("get thick", "original", "thin_image", thick_img)
-        return cp(thick_img)
         
     def Skin(self, img):
         result = self.erosionImg(img)
@@ -84,7 +71,6 @@ class MorphologyDealing(object):
         result = self.dilateImg(result)
         plt.figure()
         plt.imshow(result, 'gray')
-        return cp(result)
 
     def binaryGet(self, img, threshold):
         ret, binary_img = cv2.threshold(img, threshold, 1, cv2.THRESH_BINARY)
@@ -97,13 +83,13 @@ if __name__ == '__main__':
     #  res=md.erosionImg(img)
     #  plt.figure()
     #  plt.imshow(res,'gray')
-    md.dilateImg(img)
-    md.ExopenImg(img)
-    md.ExcloseImg(img)
-    md.Hull(img)
-    md.Skeletionize(img)
-    md.Labeled(img)
+    #  md.dilateImg(img)
+    #  md.ExopenImg(img)
+    #  md.ExcloseImg(img)
+    #  md.Hull(img)
+    #  md.Skeletionize(img)
+    #  md.Labeled(img)
     md.Thin(img)
     md.Thick(img)
-    md.Skin(img)
+    #  md.Skin(img)
     plt.show()
