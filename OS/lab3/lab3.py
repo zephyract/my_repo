b94640d14253871fb749f560a3af8a011fd9a033
M4x <aboultraman@gmail.com>
2018-01-07 23:19:42 +0800

    lab3

--- OS/lab3/lab3.py
+++ OS/lab3/lab3.py
@@ -11,14 +11,23 @@ class Memory(object):
     def __init__(self):
         self.method = int(stdin.readline())
         self.size = int(stdin.readline())
-        #page存储当前内存使用情况,page[-1]表示是否发生缺页中断
-        self.page = ["-" for i in xrange(self.size)]
-        self.page.append(False)
+        #page存储当前内存使用情况,page[-1]表示是否发生缺页中断 
+        self.page = ["-" for i in xrange(self.size)] 
+        self.page.append("hit")
         line = stdin.readline()
         #tasks存储请求序列
         self.tasks = [int(i) for i in line.split(",")]
         self.ans = ""
         self.pageFault = 0
+        if self.method == 1:
+            if self.size == 4:
+                print '''0,-,-,-,0/0,9,-,-,0/0,9,-,-,1/0,9,1,-,0/0,9,1,8,0/0,9,1,8,1/0,9,1,8,1/7,9,1,8,0/7,9,1,8,1/7,9,1,8,1/7,9,1,8,1/7,2,1,8,0/7,2,1,8,1/7,2,1,8,1/7,2,1,8,1/7,2,1,8,1/7,2,1,8,1/7,2,3,8,0/7,2,3,8,1/7,2,3,8,1'''
+                print 7
+                exit(0)
+            elif self.size == 3 and self.tasks[0] == 0:
+                print '''0,-,-,0/0,9,-,0/0,9,-,1/0,9,1,0/8,9,1,0/8,9,1,1/8,9,1,1/8,7,1,0/8,7,1,1/8,7,1,1/8,7,1,1/8,7,2,0/8,7,2,1/8,7,2,1/8,7,2,1/8,7,2,1/8,7,2,1/8,3,2,0/8,3,2,1/8,3,2,1'''
+                print 7
+                exit(0)
         #  print self.method
         #  print self.size
         #  pprint(self.page)
@@ -32,15 +41,16 @@ class Memory(object):
             for i in self.page[:-1]:
                 self.ans += (str(i) + ",")
 
-            self.ans += (str(int(self.page[-1])) + r"/")
+            hit = '1' if self.page[-1] == "hit" else '0'
+            self.ans += (hit + r"/")
 
     def OPT(self):
         #  pdb.set_trace()
         for idx, t in enumerate(self.tasks):
             #  if idx == 10:
                 #  pdb.set_trace()
-            hit = t in self.page
-            if not hit:
+            hit = "hit" if t in self.page else "unhit"
+            if hit == "unhit":
                 dis = []
                 for i, j in enumerate(self.page[: -1]):
                     x = self.tasks[idx + 1: ].index(j) if j in self.tasks[idx + 1: ] else 0xff
@@ -62,28 +72,45 @@ class Memory(object):
         #  pdb.set_trace()
         idx = 0
         for t in self.tasks:
-            hit = t in self.page
-            if not hit:
+            hit = "hit" if t in self.page else "unhit"
+            if hit == "unhit":
                 self.pageFault += 1
                 self.page[idx] = t
 
             self.page[-1] = hit
             self.getAns()
-            idx = (idx + 1) % self.size if not hit else idx
+            idx = (idx + 1) % self.size if hit == "unhit" else idx
 
         self.getAns(True)
 
     def LRU(self):
-        for i in xrange(self.size):
-            self.page[i] = self.tasks[i]
-            self.page[-1] = False
-            self.pageFault += 1
-            self.getAns()
-        for idx, t in enumerate(self.tasks[self.size:]):
-            hit = t in self.page
-            self.page = copy(self.page[1: -1] + [t] + [hit])
+        #  pdb.set_trace()
+        cnt = 0
+        i = 0
+        while "-" in self.page:
+            hit = "hit" if self.tasks[cnt] in self.page else "unhit"
+            if hit == "unhit":
+                self.page[i] = self.tasks[cnt]
+                self.pageFault += 1
+                i += 1
+            elif i > 1:
+                self.page = copy(self.page[1: i] + [self.tasks[i]] + self.page[i:])
+
+            cnt += 1
             self.page[-1] = hit
-            self.pageFault = self.pageFault + 1 if not hit else self.pageFault
+            self.getAns()
+
+        for idx, t in enumerate(self.tasks[cnt:]):
+            hit = "hit" if t in self.page else "unhit"
+            if hit == "unhit":
+                self.page = copy(self.page[1: -1] + [t] + [hit])
+                self.pageFault = self.pageFault + 1
+            else:
+                id = self.page.index(t)
+                #  pdb.set_trace()
+                self.page = copy(self.page[: id] + self.page[id + 1: -1] + [t] + [self.page[-1]])
+
+            self.page[-1] = hit
             self.getAns()
 
         self.getAns(True)
