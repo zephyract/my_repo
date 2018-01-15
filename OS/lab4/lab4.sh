5cab85a06f5302eea74af6f2197facf368f280fb
M4x <aboultraman@gmail.com>
2018-01-16 05:28:06 +0800

    lab4 bash

--- OS/lab4/lab4.sh
+++ OS/lab4/lab4.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
-set -uo pipefail
+
+set -u
+echo "Welcome to M4x's fileSystem."
+echo "This is a simplified fileSystem for lab4 of OSofBIT."
+trap 'onCtrlC' INT
+function onCtrlC () {
+	echo "Something wrong occured. Your tasks won't be saved..."
+	mv lab4.sh ../
+	rm * -r
+	mv ../lab4.sh .
+	exit
+}
 
 #转化为相对路径
 path=$(pwd)
 len=${#path}
 # echo $len
+#标记是否打开了文件系统
+flag=false
 while true
 do
 	path=$(pwd)
@@ -15,8 +28,9 @@ do
 		#echo "test"
 		mkdir "${cmd:4}"
 	elif [[ "$cmd" == sfs\ * ]]; then
+		$flag=True
 		cd "${cmd:4}"
-	elif [[ "$cmd" == mkdir\ * ]] || [[ "$cmd" == rmdir\ * ]] || [[ "$cmd" == ls ]] || [[ "$cmd" == cd\ * ]]; then
+	elif flag && ([[ "$cmd" == mkdir\ * ]] || [[ "$cmd" == rmdir\ * ]] || [[ "$cmd" == ls ]] || [[ "$cmd" == cd\ * ]]); then
 		$cmd
 	elif [[ "$cmd" == create\ *  ]]; then
 		touch "${cmd:7}"
@@ -36,6 +50,7 @@ do
 	elif [[ "$cmd" == delete\ * ]]; then
 		rm "${cmd:7}"
 	elif [[ "$cmd" == exit ]]; then
+		echo "Good Bye!"
 		exit
 	else
 		echo "Wrong command!"
