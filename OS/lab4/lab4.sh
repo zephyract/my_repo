353f066bea6917ff7f867f33cd4df26d7084a102
M4x <aboultraman@gmail.com>
2018-01-16 17:47:32 +0800

    lab4 bash

--- OS/lab4/lab4.sh
+++ OS/lab4/lab4.sh
@@ -28,18 +28,18 @@ do
 	# new,转义空格
 	if [[ "$cmd" == new\ * ]]; then
 		#echo "test"
-		mkdir "${cmd:4}" 2>error.log || {
+		mkdir "${cmd:4}" 2>/dev/null || {
 		echo "The fileSystem ""${cmd:4}"" already existed. Try another name!"
 	}
 
 	elif [[ "$cmd" == sfs\ * ]]; then
 		flag=true
-		cd "${cmd:4}" 2>error.log || {
+		cd "${cmd:4}" 2>/dev/null || {
 		echo "There is no ""${cmd:4}""! Please check your fileSystem's name!"
 	}
 
 	elif [ "$flag" = true ] && ([[ "$cmd" == mkdir\ * ]] || [[ "$cmd" == rmdir\ * ]] || [[ "$cmd" == ls ]] || [[ "$cmd" == cd\ * ]]); then
-		$cmd 2>error.log || {
+		$cmd 2>/dev/null || {
 		echo "Wrong command!"
 	}
 
@@ -47,26 +47,33 @@ do
 		# create两次即为清空 
 		touch "${cmd:7}" 
 		# 此时文件有可读可执行权限 
-		chmod 555 "${cmd:7}" 2>error.log
+		chmod 555 "${cmd:7}" 2>/dev/null
 	elif [ "$flag" = true ] && [[ "$cmd" == open\ * ]]; then
 		# 此时文件具只有可写权限  
-		chmod 666 "${cmd:5}" 2>error.log || {
+		chmod 666 "${cmd:5}" 2>/dev/null || {
 		echo "The file ""${cmd:4}"" doesn't existed!"
 	}
 	elif [ "$flag" = true ] && [[ "$cmd" == close\ * ]]; then
 		# 此时文件具有可读可执行权限
-		chmod 555 "${cmd:6}" 2>error.log || {
+		chmod 555 "${cmd:6}" 2>/dev/null || {
 		echo "The file ""${cmd:4}"" doesn't existed!"
 	}
 	elif [ "$flag" = true ] && [[ "$cmd" == read\ * ]]; then
-		cat "${cmd:5}" 2>error.log || {
+		cat "${cmd:5}" 2>/dev/null || {
 		echo "The file ""${cmd:4}"" doesn't existed!"
 	}
 	elif [ "$flag" = true ] && [[ "$cmd" == write\ * ]]; then
+		# 检测文件是否存在 
+		if [ ! -e "$cmd{:6}" ]; then
+			echo "The file ""${cmd:6}"" doesn't exist!"
+		fi
+
 		read -p "Please input your content: " content
 		echo -n "$content" > "${cmd:6}"
 	elif [ "$flag" = true ] && [[ "$cmd" == delete\ * ]]; then
-		rm "${cmd:7}"
+		rm "${cmd:7}" 2>/dev/null || {
+		echo "The file ""${cmd:4}"" doesn't existed!"
+	}
 	elif [[ "$cmd" == exit ]]; then
 		echo "Good Bye!"
 		exit
